48cc8c130ba9f8487b7daa98757616cffab6f970
Mohak Talreja <92052668+mohak0011@users.noreply.github.com>
2021-11-12 00:15:41 +0530

    updated : ExpenseItem component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,16 +2,15 @@ import React from 'react';
 import './ExpenseItem.css';
 
 function ExpenseItem() {
-    return (
-        <div className='expense-item'>
-            <div>12th November 2021 </div>
-            <div classNamee='expense-item__description'>
-            <h2>Car Insurance</h2>
-            <div className='expense-item__price'>Rs 3000</div>
-            </div>
-        </div>
-    
-    )
+  return (
+    <div className="expense-item">
+      <div>Nov 12th 2021</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">Rs 3000</div>
+      </div>
+    </div>
+  );
 }
 
-export default ExpenseItem
+export default ExpenseItem;
