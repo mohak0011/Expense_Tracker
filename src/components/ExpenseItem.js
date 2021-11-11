52b360d5247522f30a7ff9e33d97a594b05b88b1
Mohak Talreja <92052668+mohak0011@users.noreply.github.com>
2021-11-12 00:04:14 +0530

    feat : adding up the Expenseitem component with its styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from 'react';
+import './ExpenseItem.css';
 
 function ExpenseItem() {
     return (
-        <div>
-            This is a ExpenseItem component
+        <div className='expense-item'>
+            <div>12th November 2021 </div>
+            <div classNamee='expense-item__description'>
+            <h2>Car Insurance</h2>
+            <div className='expense-item__price'>Rs 3000</div>
+            </div>
         </div>
+    
     )
 }
 
