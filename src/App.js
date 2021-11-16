58c15227bae421cb057ecc60587e6be90d611b0d
Mohak Talreja <92052668+mohak0011@users.noreply.github.com>
2021-11-16 22:57:09 +0530

    Intializing :  Expense_tracker App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses/Expenses';
 
-function App() {
+const App = () => {
+
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2021,10, 14)
+    },
+    { id: 'e2', title: 'New TV', amount: 49949, date: new Date(2021, 7, 12) },
+
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    }, 
+     {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 4500,
+      date: new Date(2021, 5, 12),
+    },
+    {
+      id: 5,
+      title : 'Electricity Bill',
+      amount : 8000,
+      date : new Date(2021,10,16)
+    }
+  ] 
+
   return (
     <div>
-    <ExpenseItem/>
+     <Expenses items={expenses}/>
     </div>
   );
 }
